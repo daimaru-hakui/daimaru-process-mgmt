509756bb2cfc826b8da14bdd057154f528a50d55
daimaru-hakui <dh.mukai.dh@gmail.com>
2023-09-05 12:25:44 +0900

    gantt finish

--- src/pages/AddTask.tsx
+++ src/pages/AddTask.tsx
@@ -2,23 +2,27 @@ import { Container, Heading, useColorModeValue } from "@chakra-ui/react";
 import TaskForm from "../components/TaskForm";
 
 const AddTask = () => {
-  const bg = useColorModeValue('white', 'gray.700');
+  const bg = useColorModeValue("white", "gray.700");
 
   const defaultValues = {
     serialNumber: "",
     processNumber: "",
-    staffId:"",
+    staffId: "",
     customer: "",
-    productNumber:"",
+    productNumber: "",
     productName: "",
     sizeDetails: "",
     quantity: 0,
-    comment: ""
+    sp: 0,
+    cp: 0,
+    comment: "",
   };
 
   return (
     <Container p={6} bg={bg} rounded="md" shadow="md">
-      <Heading as="h2" fontSize="2xl">加工指示書登録</Heading>
+      <Heading as="h2" fontSize="2xl">
+        加工指示書登録
+      </Heading>
       <TaskForm defaultValues={defaultValues} pageType="NEW" />
     </Container>
   );
